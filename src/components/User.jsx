02c37d1230bdeede0d0522db1ccc110a3dfb4503
eldarasana <eldaras.anancenkov@gmail.com>
2025-03-13 15:14:58 +0200

    Reading url parameters with useParams simple

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,7 +1,23 @@
-
+import { useParams } from 'react-router-dom'
+ 
 
 export default function User(){
+
+
+    const url_arg = useParams()
+    const id = url_arg.id
+
+
     return(
-        <h1>DISPLAY USER</h1>
+
+
+        <>
+            
+            <h1>DISPLAY USER</h1>
+            <p>{id}</p>        
+        
+        </>
+
+
     )
 }
